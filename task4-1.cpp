4a73469a702447cd8a34559308ee06d36c270aa3
MMichal34 <115485417+MMichal34@users.noreply.github.com>
2022-11-14 18:05:32 +0100

    Create task4-1.cpp

---
+++ task4-1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+class Animal
+{
+  string species;
+  int legs_number;
+  bool marine_animal;
+  bool can_fly;
+public:
+    Animal (string sp, int le, bool mar, bool can):species (sp),
+    legs_number (le), marine_animal (mar), can_fly (can)
+  {
+  };
+  string name () = 0;
+};
+
+class Mammal:public Animal
+{
+  string genus;
+  bool tail;
+  bool opposable_thumbs;
+  bool can_swim;
+    Mammal (string sp, int le, bool mar, bool can, string ge, bool ta,
+	    bool op, bool sw):Animal (sp, le, mar, can), genus (ge),
+    tail (ta), opposable_thumbs (op), can_swim (sw)
+  {
+  };
+  string name ()
+  {
+    retrun species;
+  };
+};
+
+int main ()
+{
+  Mammal Guinea_pig ("Cavia porcellus", 4, false, false, "Cavia", false,
+		     false, false);
+
+  return 0;
+}
